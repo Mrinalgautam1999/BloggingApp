482c14b47682a0926b240454bf07361a51dc1463
Mrinal Gautam <mmrinalgautam1999@gmail.com>
2023-10-30 00:28:11 +0530

    changed

---
+++ BloggingAPP/src/main/java/com/geekster/BloggingAPP/Controller/UserController.java
@@ -0,0 +1,46 @@
+package com.geekster.BloggingAPP.Controller;
+
+
+import com.geekster.BloggingAPP.Model.DTO.SignInDto;
+import com.geekster.BloggingAPP.Model.DTO.UserSignInDTO;
+import com.geekster.BloggingAPP.Model.Post;
+import com.geekster.BloggingAPP.Service.PostService;
+import com.geekster.BloggingAPP.Service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Validated
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PostService postService;
+
+    @PostMapping("user/signUp")
+    public String userSignUp(@RequestBody @Valid UserSignInDTO userSignInDTO){
+        return userService.userSignUp(userSignInDTO);
+    }
+
+    @PostMapping("User/signIn")
+    public String userSignIn(@RequestBody SignInDto signInDto){
+        return userService.userSignIn(signInDto);
+    }
+
+    @DeleteMapping("user/signOut")
+    public String userSignOut(@RequestParam @Valid String email, @RequestParam String token)
+    {
+        return userService.userSignOut(email,token);
+    }
+
+    @GetMapping("postById/{postId}")
+    public Post getPostById(@RequestParam @Valid String email, @RequestParam @Valid String token, @PathVariable Integer postId)
+    {
+        return userService.getPostById(email, token, postId);
+    }
+
+}
